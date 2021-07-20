9dcf44f20366748ba95f77fc7cb429c85bf7f6a7
rustydb <doomslayer@hpe.com>
2021-07-20 04:34:10 -0500

    Warn on dev

--- scripts/rpm-functions.sh
+++ scripts/rpm-functions.sh
@@ -92,6 +92,7 @@ function list-compute-packages() {
 
 function install-packages() {
   if [[ "$DEV" = 'true' ]]; then
+    echo >&2 "DEV: $DEV"
     remove-comments-and-empty-lines "$1" | xargs -r zypper -n install --auto-agree-with-licenses --no-recommends --allow-unsigned-rpm
   else
     remove-comments-and-empty-lines "$1" | xargs -r zypper -n install --auto-agree-with-licenses --no-recommends
