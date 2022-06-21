d1dc9e01969ef2ad53a472b7cd2a7577f0034506
Russell Bunch <doomslayer@hpe.com>
2022-06-21 13:31:51 -0500

    MTL-1708 sles15sp4 (#463)
    * Use algol60 images
    
    * MTL-1708 Switch to SP4 repos
    
    Where necessary also include SP3 repositories to supply packages that
    aren't yet available in SP4.
    
    * MTL-1708 SLES15SP4 Updates
    
    libfmt7 needs to come from the SP3 repo still, there's no official build
    for SP4 yet.
    
    This also drops python3-sip which is no longer available, and google-osconfig-agent.
    
    * Update Jenkins options
    
    * kernel-mft-mlnx-kmp-default is based on kernel 5.3 and is incompatible with kernel 5.4, commenting out - perhaps it is not needed in the new kernel
    
    * Split Python packages
    
    Ensure python2 and python3 are installed, as well as python3's helpers
    (pip, setuptools, virtualenv, and wheel). Move all the secondary Python
    packages into their own list.
    
    * Add jq to setup function

--- build/create-index.sh
+++ build/create-index.sh
@@ -29,7 +29,7 @@ sed -e '/^Not downloading package /!d' \
     -e "s/^Not downloading package '//" \
     -e 's/[[:space:]]\+.*$//' \
 | sort -u \
-| docker run --rm -i arti.dev.cray.com/internal-docker-stable-local/packaging-tools:0.7.0 rpm-index -v \
+| docker run --rm -i  artifactory.algol60.net/csm-docker/stable/csm-docker-sle:latest rpm-index -v \
     $(cat "$@" | remove-comments-and-empty-lines | awk '{print "-d", $1, $NF}') \
 | tee "${workdir}/index.yaml"
 
